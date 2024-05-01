510d1dd24ea291ef5c3a230651f7f1a0f6dc5634
Oksana Ivanova <nyamnyamaq@yandex.ru>
2024-05-01 17:16:59 +0300

    Первая версия ПР5

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
+const button = cardTemplate.querySelector('.card__delete-button');
 
 // @todo: Функция создания карточки
+function createCard(element, deleteCard) {
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+	cardElement.querySelector('.card__image').src = element.link;
+	cardElement.querySelector('.card__image').alt = element.name;
+	cardElement.querySelector('.card__title').textContent = element.name;
+
+	cardElement.querySelector('.card__delete-button').addEventListener('click', function() {
+		deleteCard(cardElement);
+	})
+
+	return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(card) {
+	card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(element){
+	const cardNew = createCard(element, deleteCard);
+	placesList.append(cardNew);
+});
